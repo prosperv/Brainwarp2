e270cf9ed350ded82cd50dd7a562eec1e5133c83
Prosper <vanprosper@gmail.com>
2020-07-04 14:14:38 -0700

    updated dynamic anaylsis

--- Brainwarp Tilt Sensor/src/fusion.h
+++ Brainwarp Tilt Sensor/src/fusion.h
@@ -27,6 +27,14 @@ float vector_dot(const Ta *a, const Tb *b)
     return (a[0] * b[0]) + (a[1] * b[1]) + (a[2] * b[2]);
 };
 
+template <typename Ta, typename Tb, typename To>
+void vector_cross(To *out, const Ta *a, const Tb *b)
+{
+    out[0] = (a[1] * b[2]) - (a[2] * b[1]);
+    out[1] = (a[2] * b[0]) - (a[0] * b[2]);
+    out[2] = (a[0] * b[1]) - (a[1] * b[0]);
+};
+
 void vector_int_add(int out[], const int a[], const int b[])
 {
     out[0] = a[0] + b[0];
@@ -34,11 +42,17 @@ void vector_int_add(int out[], const int a[], const int b[])
     out[2] = a[2] + b[2];
 
     if (out[0] != 0)
-        out[0] /= out[0];
+        out[0] /= myABS(out[0]);
     if (out[1] != 0)
-        out[1] /= out[1];
+        out[1] /= myABS(out[1]);
     if (out[2] != 0)
-        out[2] /= out[2];
+        out[2] /= myABS(out[2]);
+};
+
+template <typename T>
+bool vector_equality(T *left, T *right)
+{
+    return left[0] == right[0] && left[1] == right[1] && left[2] == right[2];
 };
 
 class Fusion
@@ -68,11 +82,16 @@ public:
         _gyro.writeReg(L3G::CTRL_REG4, 0b00110000);
     };
 
-    void stop(){};
+    void stop()
+    {
+        auto byte = _gyro.readReg(L3G::CTRL_REG1);
+        byte &= ~(0b00001000);
+        _gyro.writeReg(L3G::CTRL_REG1, 0b01101111);
+    };
 
     int isOnSide(double sideToCheck, double axis1, double axis2)
     {
-        const double ZERO_TRESHOLD = 0.58;
+        const double ZERO_TRESHOLD = 0.60;
         const double G_THRESHOLD = 0.75;
 
         // We know we're on a side if one axis has a high values while the other 2 axsi are near zero.
@@ -115,40 +134,55 @@ public:
         }
     };
 
-    IMUSide dynamicAnalysis(IMUSide lastSide, unsigned long usTimeDelta, double gryoValue[])
+    bool dynamicAnalysis(int vector[3], const int lastVector[3], const unsigned long usTimeDelta, const double gryoValue[])
     {
+        bool ret = false;
         double gryoValueCopy[] = {gryoValue[0],
                                   gryoValue[1],
                                   gryoValue[2]};
 
+        // Need to know the current toy orientation else we exit.
+        // if (vector_dot(lastVector, lastVector) == 0)
+        //     return;
+
         // Zero out the axis if the toy is already on that axis, since the toy does not make
         // moves to the opposite ends, and we don't care about landing on the same side, since
         // the static analysis will handle that.
-        switch (lastSide)
-        {
-        case IMUSide::xMinus:
-        case IMUSide::xPlus:
+        if (lastVector[0] != 0)
             gryoValueCopy[0] = 0;
-            break;
-        case IMUSide::yMinus:
-        case IMUSide::yPlus:
+        if (lastVector[1] != 0)
             gryoValueCopy[1] = 0;
-            break;
-        case IMUSide::zMinus:
-        case IMUSide::zPlus:
+        if (lastVector[2] != 0)
             gryoValueCopy[2] = 0;
-            break;
-        default:
-            return IMUSide::None;
-            break;
-        }
 
         const auto timeDelta = usTimeDelta / 1000000.0;
-        _accumulatedDegree[0] = gryoValueCopy[0] * timeDelta;
-        _accumulatedDegree[1] = gryoValueCopy[1] * timeDelta;
-        _accumulatedDegree[2] = gryoValueCopy[2] * timeDelta;
+        _accumulatedDegree[0] += gryoValueCopy[0] * timeDelta;
+        _accumulatedDegree[1] += gryoValueCopy[1] * timeDelta;
+        _accumulatedDegree[2] += gryoValueCopy[2] * timeDelta;
 
-        // const double DEGREE_THRESHOLD = 30;
+        const double DEGREE_THRESHOLD = 50;
+        if (myABS(_accumulatedDegree[0]) > DEGREE_THRESHOLD)
+        {
+            int value = _accumulatedDegree[0] > 0 ? 1 : -1;
+            int xUnitVector[3] = {value, 0, 0};
+            vector_cross(vector, lastVector, xUnitVector);
+            ret = true;
+        }
+        else if (myABS(_accumulatedDegree[1]) > DEGREE_THRESHOLD)
+        {
+            int value = _accumulatedDegree[1] > 0 ? 1 : -1;
+            int yUnitVector[3] = {0, value, 0};
+            vector_cross(vector, lastVector, yUnitVector);
+            ret = true;
+        }
+        else if (myABS(_accumulatedDegree[2]) > DEGREE_THRESHOLD)
+        {
+            int value = _accumulatedDegree[2] > 0 ? 1 : -1;
+            int zUnitVector[3] = {0, 0, value};
+            vector_cross(vector, lastVector, zUnitVector);
+            ret = true;
+        }
+        return ret;
     };
 
     void computeScaledGyro(double scaledGyro[3], L3G::vector<int16_t> g)
@@ -168,7 +202,7 @@ public:
         double scaledAccel[3];
         double scaledGyro[3];
         auto currentReadTime = micros();
-        auto readTimeDelta = _lastReadTime - currentReadTime;
+        auto readTimeDelta = currentReadTime - _lastReadTime;
 
         _accel.get_Gxyz(scaledAccel);
         _gyro.read();
@@ -176,20 +210,42 @@ public:
 
         /// Static: Toy is not moving and is stable
         int staticVector[3] = {0, 0, 0};
-        staticAnalysis(staticVector, scaledAccel, scaledGyro);
-        int dynamicVector[3] = {0, 0, 0};
-        // dynamicAnalysis(dynamicVector, _lastValidSide, readTimeDelta, scaledGyro);
+        bool staticStatus = staticAnalysis(staticVector, scaledAccel, scaledGyro);
 
-        vector_int_add(_lastValidSide, staticVector, dynamicVector);
+        bool print = false;
+        const double GYRO_MOVEMENT_THRESHOLD = 100;
+        auto gyroMagnitude = sqrt(vector_dot(scaledGyro, scaledGyro));
+
+        if (gyroMagnitude < GYRO_MOVEMENT_THRESHOLD && staticStatus)
+        {
+            resetAccumulatedDegree();
+        };
+
+        int dynamicVector[3] = {0, 0, 0};
+        bool dynamicStatus = dynamicAnalysis(dynamicVector, _lastValidSide, readTimeDelta, scaledGyro);
+
+        vector_int_add(_lastSide, staticVector, dynamicVector);
+        if (vector_dot(_lastSide, _lastSide) == 1)
+        {
+            if (!vector_equality(_lastValidSide, _lastSide))
+            {
+                resetAccumulatedDegree();
+            }
+            _lastValidSide[0] = _lastSide[0];
+            _lastValidSide[1] = _lastSide[1];
+            _lastValidSide[2] = _lastSide[2];
+        }
         _lastReadTime = currentReadTime;
 
-        auto totalProcessTime = currentReadTime - micros();
-        return _lastValidSide;
+        auto totalProcessTime = micros() - currentReadTime;
+
+        return _lastSide;
     }
 
     L3G _gyro;
     ADXL345 _accel = ADXL345();
-    int _lastValidSide[3] = {0, 0, 0};
+    int _lastSide[3] = {1, 0, 0};
+    int _lastValidSide[3] = {1, 0, 0};
     unsigned long _lastReadTime = micros();
     double _accumulatedDegree[3] = {0, 0, 0};
 };
